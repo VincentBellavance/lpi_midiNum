cc3d2aeaac666526d7ce07ef619697e0df82b7bb
Katherine Hébert <katherine.ahebert@gmail.com>
2020-11-03 18:01:22 -0500

    initial commit

---
+++ scripts/lpi_demo.R
@@ -0,0 +1,16 @@
+## LPI demonstration
+
+# load required packages
+library(tidyverse)
+library(mgcv)
+library(boot)
+
+# Generate population time series ----------------------------------------------
+
+# Fit model on each population's abundance through time ------------------------
+
+# Predict abundance over time period covered by the index ----------------------
+
+# Take average per species per group -------------------------------------------
+
+# Calculate index based on baseline (LPI = 1 in 1970) --------------------------
