3a95247015c5656f89060aa8a86774db4fc9dcc3
Katherine Hébert <katherine.ahebert@gmail.com>
2020-11-06 14:06:57 -0500

    finish up methodology

--- scripts/lpi_demo.R
+++ scripts/lpi_demo.R
@@ -23,10 +23,12 @@ ggplot(df_l) +
   geom_line(aes(x = year, y = N, col = population)) +
   facet_grid(taxa ~ system) +
   coord_cartesian(ylim = c(0,max(df_l$N+10))) +
-  labs(y = "N(t)", 
+  labs(y = "Abundance", 
        col = "Populations", 
        title = "Step 1: Population time series") +
+  scale_color_brewer(palette = "Dark2") +
   theme(legend.position = "bottom")
+ggsave("images/fig1_timeseries.png", width = 6, height = 6, units = "in")
 
 # Fit model on each population's abundance through time ------------------------
 
@@ -37,7 +39,7 @@ pops <- split(df_l, df_l$population)
 # create list to store models
 m <- vector("list", length(pops))
 # fit models
-for(i in 1:length(gams)){
+for(i in 1:length(m)){
   m[[i]] <- gam(N_log10 ~ s(year, k = 10), # k = half the length of the time series
                    data = pops[[i]],
                    family = gaussian(), 
@@ -76,6 +78,7 @@ ggplot(df_l, aes(x = year, group = population)) +
        caption = "Ribbon shows standard error from GAM predictions.") + 
   facet_grid(taxa ~ system) +
   theme(legend.position = "bottom")
+ggsave("images/fig2_gam.png", width = 6, height = 6, units = "in")
 
 
 # Calculate each population's growth rate (dt) through time --------------------
@@ -95,39 +98,144 @@ ggplot(df_l, aes(x = year, col = population)) +
        title = "Step 3: Calculate each population's annual growth rate") +
   facet_grid(taxa ~ system) +
   theme(legend.position = "bottom")
+ggsave("images/fig3_growthrates.png", width = 6, height = 6, units = "in")
 
-
-# Take average annual growth rate per taxonomic group --------------------------
-
-# subset to mammals and split by year
-mammals <- df_l[which(df_l$taxa == "mammals"),]
-
-# subset to birds and split by year
-birds <- df_l[which(df_l$taxa == "birds"),]
+# Take average annual growth rate per taxonomic group per system ---------------
 
 # create df to store results
-mean_mammals <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
-mean_birds <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
+mammals_marine <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
+birds_marine <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
+mammals_terrestrial <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
+birds_terrestrial <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
 
 # calculate geometric mean growth rate 
 # with 95% CI from bootstrapping
 for(t in 2:length(years)){
   
   # birds, marine
-  dt_birds_marine <- filter(birds, year == years[t]) %>% 
-    filter(system == "marine") %>% 
-    na.omit()
-  birds_marine[t,] <- dt_boot(10^dt_birds$dt) 
+  dt_temp <- na.omit(df_l) %>% 
+    filter(taxa == "birds", system == "marine", year == years[t]) 
+  birds_marine[t,] <- dt_boot(10^dt_temp$dt) 
   
+  # mammals, marine
+  dt_temp <- na.omit(df_l) %>% 
+    filter(taxa == "mammals", system == "marine", year == years[t]) 
+  mammals_marine[t,] <- dt_boot(10^dt_temp$dt) 
   
-  # mammals
-  dt_mammals <- filter(birds, year == years[t]) %>% na.omit()
-  mean_mammals[t,] <- dt_boot(10^dt_mammals$dt) 
+  # birds, terrestrial
+  dt_temp <- na.omit(df_l) %>% 
+    filter(taxa == "birds", system == "terrestrial", year == years[t]) 
+  birds_terrestrial[t,] <- dt_boot(10^dt_temp$dt) 
+  
+  # mammals, terrestrial
+  dt_temp <- na.omit(df_l) %>% 
+    filter(taxa == "mammals", system == "terrestrial", year == years[t]) 
+  mammals_terrestrial[t,] <- dt_boot(10^dt_temp$dt) 
   
 }
 
-# dt_gm = bind_rows(dt_gm) %>% log10() %>% 
-#   mutate(time = unique(dt_df$time))
+# join the data frames together
+birds_marine <-  mutate(birds_marine, 
+                        year = years, taxa = "birds", system = "marine")
+mammals_marine <- mutate(mammals_marine, 
+                         year = years, taxa = "mammals", system = "marine")
+birds_terrestrial <- mutate(birds_terrestrial, 
+                            year = years, taxa = "birds", system = "terrestrial")
+mammals_terrestrial <- mutate(mammals_terrestrial, 
+                              year = years, taxa = "mammals", system = "terrestrial")
+taxa_system <- rbind(birds_marine, mammals_marine, birds_terrestrial, mammals_terrestrial)
 
-# Calculate index based on baseline (LPI = 1 in 1970) --------------------------
+# plot the taxa_system group means 
+ggplot(taxa_system, aes(x = year)) +
+  geom_ribbon(aes(ymin = cilo, ymax = cihi, fill = taxa), alpha = .3) +
+  geom_line(aes(y = dt_mean, col = taxa)) +
+  scale_fill_brewer(palette = "Dark2") +
+  scale_color_brewer(palette = "Dark2") +
+  labs(y = "Mean annual growth rate", 
+       col = "Taxonomic group", fill = "Taxonomic group", 
+       title = "Step 4: Calculate the mean annual growth rate (Part 1)",
+       subtitle = "Mean growth rate within each taxonomic group in each system") +
+  facet_grid(taxa ~ system) +
+  theme(legend.position = "bottom")
+ggsave("images/fig4_mean_taxasystem.png", width = 6, height = 6, units = "in")
 
+
+# Take mean annual growth rate per system --------------------------------------
+
+# create df to store results
+marine <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
+terrestrial <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
+
+# calculate geometric mean growth rate 
+for(t in 1:length(years)){
+  
+  # marine
+  dt_temp <- taxa_system %>% filter(system == "marine", year == years[t]) 
+  marine[t,1:3] <- apply(select(dt_temp, c(dt_mean, cilo, cihi)), 2, function(x) gm_mean(10^x))
+  
+  # terrestrial
+  dt_temp <- taxa_system %>% filter(system == "terrestrial", year == years[t]) 
+  terrestrial[t,1:3] <- apply(select(dt_temp, c(dt_mean, cilo, cihi)), 2, function(x) gm_mean(10^x))
+}
+
+# join the data frames together
+marine <-  mutate(marine, year = years, system = "marine")
+terrestrial <- mutate(terrestrial,  year = years, system = "terrestrial")
+system_df <- rbind(marine, terrestrial)
+
+# plot the system group means 
+ggplot(system_df, aes(x = year, group = system)) +
+  geom_ribbon(aes(ymin = cilo, ymax = cihi), alpha = .3) +
+  geom_line(aes(y = dt_mean)) +
+  scale_color_brewer(palette = "Set1") +
+  labs(y = "Mean annual growth rate", 
+       col = "System", 
+       title = "Step 4: Calculate the mean annual growth rate (Part 2)",
+       subtitle = "Mean growth rate within each system") +
+  facet_wrap(~system) +
+  theme(legend.position = "bottom")
+ggsave("images/fig5_mean_system.png", width = 6, height = 6, units = "in")
+
+  
+# Take average annual growth rate globally -------------------------------------
+
+# create df to store results
+all <- data.frame(dt_mean = 1, cilo = 1, cihi = 1)
+
+# calculate geometric mean growth rate 
+for(t in 1:length(years)){
+    dt_temp <- system_df %>% filter(year == years[t]) 
+  all[t,1:3] <- apply(select(dt_temp, c(dt_mean, cilo, cihi)), 2, function(x) gm_mean(log10(x)))
+}
+
+# join the data frames together
+all <-  mutate(all, year = years)
+
+# plot the system group means 
+ggplot(all, aes(x = year)) +
+  geom_ribbon(aes(ymin = cilo, ymax = cihi), alpha = .3) +
+  geom_line(aes(y = dt_mean)) +
+  labs(y = "Mean annual growth rate", 
+       title = "Step 4: Calculate the mean annual growth rate (Part 3)",
+       subtitle = "Mean growth rate across systems") +
+  theme(legend.position = "bottom")
+ggsave("images/fig6_mean_all.png", width = 6, height = 6, units = "in")
+
+
+# Calculate Living Planet Index (LPI baseline = 1) -----------------------------
+
+# calculate LPI with confidence intervals
+lpi <- data.frame(
+  year = years, 
+  lpi = calclpi(log10(all$dt_mean)),
+  cilo = calclpi(log10(all$cilo)),
+  cihi = calclpi(log10(all$cihi))
+)
+
+# plot
+ggplot(lpi, aes(x = year)) +
+  geom_ribbon(aes(ymin = cilo, ymax = cihi), alpha = .3) +
+  geom_line(aes(y = lpi)) +
+  labs(y = "Living Planet Index", 
+       title = "Step 5: Calculate the Living Planet Index")
+ggsave("images/fig7_lpi.png", width = 6, height = 6, units = "in")
